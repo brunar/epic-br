4d146ebbdedf2cbf72ecee97b292c71d0b6a70c2
brunar <brunaradeke@hotmail.com>
2025-12-10 15:34:54 +0000

    Refs - solution exercise

--- src/app/hooks/refs/page.tsx
+++ src/app/hooks/refs/page.tsx
@@ -1,21 +1,37 @@
 'use client';
 import { useState } from 'react';
-// import VanillaTilt from 'vanilla-tilt';
+import VanillaTilt from 'vanilla-tilt';
 
-// interface HTMLVanillaTiltElement extends HTMLDivElement {
-//   vanillaTilt?: VanillaTilt;
-// }
+interface HTMLVanillaTiltElement extends HTMLDivElement {
+  vanillaTilt?: VanillaTilt;
+}
 
-// const vanillaTiltOptions = {
-//   max: 25,
-//   speed: 400,
-//   glare: true,
-//   'max-glare': 0.5,
-// };
+const vanillaTiltOptions = {
+  max: 25,
+  speed: 400,
+  glare: true,
+  'max-glare': 0.5,
+};
 
 function Tilt({ children }: { children: React.ReactNode }) {
   return (
-    <div className="tilt-root">
+    <div
+      className="tilt-root mt-4"
+      ref={(tiltNode: HTMLVanillaTiltElement | null) => {
+        // 🦉 The types show tiltNode can be null. This is for backward
+        // compatibility reasons and will be removed in the future.
+        console.log({ tiltNode });
+        if (!tiltNode) return;
+
+        VanillaTilt.init(tiltNode, vanillaTiltOptions);
+
+        // Cleanup function
+        return () => {
+          console.log('cleanup');
+          tiltNode.vanillaTilt?.destroy();
+        };
+      }}
+    >
       <div className="tilt-child">{children}</div>
     </div>
   );
@@ -31,7 +47,7 @@ export default function RefsExample() {
         <Tilt>
           <div className="totally-centered">
             <button
-              className="count-button"
+              className="count-button text-gray-900"
               onClick={() => setCount((c) => c + 1)}
             >
               {count}
