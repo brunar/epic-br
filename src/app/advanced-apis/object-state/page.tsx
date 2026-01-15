30b35af560da35c8e783f73e0cd43ac94224480a
brunar <brunaradeke@hotmail.com>
2026-01-15 20:40:11 +0000

    Advanced Apis - State Object - useReducer - complement info

--- src/app/advanced-apis/object-state/page.tsx
+++ src/app/advanced-apis/object-state/page.tsx
@@ -5,7 +5,7 @@ type State = { count: number };
 type Action = State;
 
 function countReducer(state: State, action: Action) {
-  return { ...state, ...action };
+  return { ...state, ...action }; // *** Best way *** for spread operator
 }
 
 type CounterProps = {
@@ -26,19 +26,25 @@ function Counter({ initialCount = 0, step = 1 }: CounterProps) {
   const increment = () => setState({ count: count + step });
   const decrement = () => setState({ count: count - step });
 
-  // <code>
-  //     const decrement = () => setState({ ...state, count: count - step });
-  // </code>
+  // OTHER EXAMPLE how to apply the spread operator and don't loose the second object value when clicked in the function
+  // The best ways is showing above at line 8
+  //    const decrement = () => setState({ count: count - step, someOtherState: state.someOtherState });
+  //    const decrement = () => setState({ ...state, count: count - step }); // better than above
 
   return (
     <div className="mt-4">
       <output className="mb-4 inline-block bg-gray-100 px-4 py-2 rounded-lg">
         {count}
       </output>
-      <div className="flex gap-2">
+      <div className="flex gap-2 mb-8">
         <button onClick={decrement}>←</button>
         <button onClick={increment}>→</button>
       </div>
+      <h4>
+        View the code to see info about the <b>spread operator</b> object and
+        the ways to apply it
+      </h4>
+      <pre>{state ? JSON.stringify(state, null, 2) : 'No data'}</pre>
     </div>
   );
 }
