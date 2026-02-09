9395d9f561ebfc7592d4b23d6b49f2ea9411fc51
brunar <brunaradeke@hotmail.com>
2026-02-09 17:35:23 +0000

    Advanced Apis - flushSync() - solution

--- src/app/advanced-apis/flush-sync/page.tsx
+++ src/app/advanced-apis/flush-sync/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useRef, useState } from 'react';
+import { flushSync } from 'react-dom';
 
 function EditableText({
   id,
@@ -18,6 +19,7 @@ function EditableText({
   const [value, setValue] = useState(initialValue);
   const inputRef = useRef<HTMLInputElement>(null);
   // 🐨 add a button ref here
+  const buttonRef = useRef<HTMLInputElement>(null);
 
   return edit ? (
     <form
@@ -26,9 +28,12 @@ function EditableText({
         event.preventDefault();
         // here's where you'd send the updated value to the server
         // 🐨 wrap these calls in a flushSync
-        setValue(inputRef.current?.value ?? '');
-        setEdit(false);
+        flushSync(() => {
+          setValue(inputRef.current?.value ?? '');
+          setEdit(false);
+        });
         // 🐨 after flushSync, focus the button with the button ref
+        buttonRef.current?.focus();
       }}
     >
       <input
@@ -42,15 +47,21 @@ function EditableText({
         onKeyDown={(event) => {
           if (event.key === 'Escape') {
             // 🐨 wrap this in a flushSync
-            setEdit(false);
+            flushSync(() => {
+              setEdit(false);
+            });
             // 🐨 after the flushSync, focus the button
+            buttonRef.current?.focus();
           }
         }}
         onBlur={(event) => {
           // 🐨 wrap these in a flushSync
-          setValue(event.currentTarget.value);
-          setEdit(false);
+          flushSync(() => {
+            setValue(event.currentTarget.value);
+            setEdit(false);
+          });
           // 🐨 after the flushSync, focus the button
+          buttonRef.current?.focus();
         }}
       />
     </form>
@@ -58,11 +69,16 @@ function EditableText({
     <button
       aria-label={buttonLabel}
       // 🐨 add a ref prop for the button
+      //Fix the type error by asserting the type of the ref
+      ref={buttonRef as React.RefObject<HTMLButtonElement>}
       type="button"
       onClick={() => {
         // 🐨 wrap this in a flushSync
-        setEdit(true);
+        flushSync(() => {
+          setEdit(true);
+        });
         // 🐨 after the flushSync, select all the text of the input
+        inputRef.current?.select();
       }}
     >
       {value || 'Edit'}
@@ -75,8 +91,16 @@ function PageFlushSync() {
     <>
       <h2>flushSync </h2>
       <p>From React DOM</p>
-
-      <div className="flex flex-col gap-4 p-4">
+      <ul className="mb-4">
+        <li>Focus on button Unamed when input submit [Enter key]</li>
+        <li>
+          Focus on button Unamed on input blur (to blur click on [tab key] after
+          editing input)
+        </li>
+        <li>Focus on button Unamed when input escape key [ESC]</li>
+        <li>Focus on Input if click on Button</li>
+      </ul>
+      <div className="flex flex-col gap-4">
         <button>First button - focus before</button>
 
         <div>
